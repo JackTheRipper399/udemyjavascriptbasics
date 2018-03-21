286431447055fe439f0da00712fe4f285f49d3c2
JackTheRipper399 <mike.morin.alsbury@gmail.com>
2018-03-21 18:53:26 -0400

    difference between dot and bracket notation

---
+++ accessproperties.js
@@ -0,0 +1,21 @@
+var snowman = {
+isSnow: true,
+color: 'white',
+madeOfSnowballs: 3,
+'first name': 'Frosty'
+};
+
+snowman.isSnow;
+snowman.color;
+snowman.madeOfSnowballs;
+snowman['first name'];
+// dot notation does not work with invalid variable names
+
+var house = {};
+house.windows = 4
+house.door = 2
+house.rainproofRoof = true
+house.material = 'stone'
+house['year origin'] = 1990
+house['door material'] = 'wood'
+house['air conditioning'] = true
