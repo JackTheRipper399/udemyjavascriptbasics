0776f404e42275a42dedd8683c6b2eb4dc2eee37
JackTheRipper399 <mike.morin.alsbury@gmail.com>
2018-03-21 17:57:35 -0400

    Learning what objects are

---
+++ objects.js
@@ -0,0 +1,19 @@
+// literal notation
+var person = {
+hair: 'brown',
+eyes: 2,
+alive: true
+};
+
+// dot notation
+var pizza = {};
+pizza.crust = 'dough';
+pizza.cheese = 'mozarella';
+pizza.sauce = 'tomato sauce';
+pizza.topping = 'bacon'
+
+// bracket notation
+var cup = {};
+cup['insulated'] = true
+cup['oz'] = 12
+cup['liquid'] = 'tea'
