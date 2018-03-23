67b0b0c21cd6f8be5c04de5ded191d2f6e9b12c2
JackTheRipper399 <mike.morin.alsbury@gmail.com>
2018-03-23 14:08:57 -0400

    Added function exercises

---
+++ functions.js
@@ -0,0 +1,15 @@
+function goodMorningNeighbor() {
+    return 'Good morning, neighbor!';
+}
+
+function goodEveningNeighbor() {
+    return 'Good evening, neighbor, thnaks for everything!';
+}
+
+function goodMorningNeighbor(name) {
+    return 'Good morning, ' + name + '!';
+}
+
+function goodEveningNeighbor(name, help) {
+    return 'Good evening, ' + name + ', thanks for ' + help + '!';
+}
