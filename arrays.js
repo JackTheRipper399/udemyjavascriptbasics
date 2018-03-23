e889655ead7f84a2e09c57e059915e5f1276b1cb
JackTheRipper399 <mike.morin.alsbury@gmail.com>
2018-03-23 11:09:30 -0400

    Added array exercises

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var baseballTeams = ['Dodgers', 'Giants', 'Mets', 'Yankees', 'Astros'];
+
+baseballTeams.push('Rangers');
+
+baseballTeams[1] = 'Cardinals';
+
+var myFavoriteTeam = baseballTeams[0];
+
+baseballTeams.length
