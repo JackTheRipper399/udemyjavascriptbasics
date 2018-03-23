cc4817d7eec74fa403e0f86b1e01f4c0e8707144
JackTheRipper399 <mike.morin.alsbury@gmail.com>
2018-03-23 11:27:30 -0400

    Added array method exercises

---
+++ arraymethods.js
@@ -0,0 +1,9 @@
+var shoppingList = ['apples', 'pizza', 'chicken', 'mushrooms', 'oranges', 'beef'];
+
+shoppingList.push('grapes');
+shoppingList.unshift('crackers');
+shoppingList.shift();
+shoppingList.pop();
+
+//pop and push are for the end
+//shift and unshift are for the beginning
